5d188a4081c7c512e989a0a7c622466dceee41a7
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2020-10-19 01:18:07 +0900

    eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import { AppProps } from 'next/app'
+import React from 'react';
+import { AppProps } from 'next/app';
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps): React.ReactElement {
+  return <Component {...pageProps} />;
 }
 
-export default App
+export default App;
